b2e03a9d89086eb7e8e2e51a48135615ff5d328d
AagamanMainali <111833078+AagamanMainali35@users.noreply.github.com>
2024-09-27 15:41:36 +0545

    Delete chatbot_project directory

--- chatbot_project/chatbot_project/chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import train_chatbot, chat  # Ensure you import the new view
-
-urlpatterns = [
-    path("train/", train_chatbot, name="train_chatbot"),
-    path("chat/", chat, name="chat"),  
-]
